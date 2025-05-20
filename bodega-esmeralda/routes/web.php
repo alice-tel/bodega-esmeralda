6d495cda53e28d8169e2523719818b1e1744512b
alice <a.tel@st.hanze.nl>
2025-05-20 12:06:21 +0200

    Default page is now the login page

--- bodega-esmeralda/routes/web.php
+++ bodega-esmeralda/routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+Route::get('/welcome', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
