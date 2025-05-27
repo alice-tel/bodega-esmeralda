26e0e917c35b914fd77f814b3fbd4ae6d4c2a2d2
Tubsy <80598416+Tubsy@users.noreply.github.com>
2025-05-27 11:38:47 +0200

    Added colors and fonts in tailwind.config.js

--- bodega-esmeralda/tailwind.config.js
+++ bodega-esmeralda/tailwind.config.js
@@ -13,7 +13,28 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                // for light, semibold etc refer with font-poppins font-semibold
+                poppins: ['Poppins', 'sans-serif'],
+                // Rare details
+                lora: ['Lora', 'serif'],
+            },
+            fontSize: {
+                base: '10px',
+                lg: '14px',
+            },
+            colors: {
+                primary: {
+                    100: '#eeebe3',
+                },
+                secondary: {
+                    100: '#2b394E',
+                },
+                font: {
+                    100: '#333333',
+                },
+                details: {
+                    100: '#CF1F25',
+                }
             },
         },
     },
