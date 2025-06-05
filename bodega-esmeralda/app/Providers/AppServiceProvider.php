36f335a305a0d017424fab056245784ff7a5c4fd
alice <a.tel@st.hanze.nl>
2025-06-05 13:34:33 +0200

    Removed code that was causing an unresolved promise, breaking the existing login

--- bodega-esmeralda/app/Providers/AppServiceProvider.php
+++ bodega-esmeralda/app/Providers/AppServiceProvider.php
@@ -22,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
-        Route::middleware('api')
-            ->prefix('api')
-            ->group(base_path('routes/api.php'));
+        // Route::middleware('api')
+        //     ->prefix('api')
+        //     ->group(base_path('routes/api.php'));
     }
 }
