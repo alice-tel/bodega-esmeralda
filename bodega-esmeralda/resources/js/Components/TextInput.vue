d3716fad7971693739c5c76755799f98940780c2
Tubsy <80598416+Tubsy@users.noreply.github.com>
2025-06-09 13:41:18 +0200

    Register styling done (bg and card not sure abt coloring?). Primary button colors changed, TextInput fixed for bottom border

--- bodega-esmeralda/resources/js/Components/TextInput.vue
+++ bodega-esmeralda/resources/js/Components/TextInput.vue
@@ -40,7 +40,7 @@ defineExpose({ focus: () => input.value.focus() });
         <input
             :id="id"
             :type="type"
-            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            class="block py-2.5 px-0 w-full text-sm text-font-100 bg-transparent border-t-0 border-l-0 border-r-0 border-b-2 border-gray-500 appearance-none focus:outline-none focus:ring-0 focus:border-details-100 peer"
             :placeholder="placeholder"
             :value="modelValue"
             @input="$emit('update:modelValue', $event.target.value)"
@@ -50,7 +50,7 @@ defineExpose({ focus: () => input.value.focus() });
         <label
             v-if="label"
             :for="id"
-            class="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-blue-600"
+            class="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-details-100"
         >
             {{ label }}
         </label>
--- bodega-esmeralda/resources/js/Pages/Auth/Register.vue
+++ bodega-esmeralda/resources/js/Pages/Auth/Register.vue
@@ -79,6 +79,7 @@ const submit = () => {
                     required
                     label="Confirm Password"
                     autocomplete="new-password"
+                    placeholder=""
                 />
 
                 <InputError
@@ -90,7 +91,7 @@ const submit = () => {
             <div class="mt-4 flex items-center justify-end">
                 <Link
                     :href="route('login')"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    class="rounded-md text-sm text-gray-500 underline hover:text-font-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                     Already registered?
                 </Link>
