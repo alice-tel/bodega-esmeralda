f2d07d38d681f263e5bea4410cc69011b2fcd133
Gustavo Costa Araújo <g.h.costa.araujo@st.hanze.nl>
2025-06-10 13:34:22 +0200

    Fix

--- bodega-esmeralda/resources/js/Pages/Diagrams.vue
+++ bodega-esmeralda/resources/js/Pages/Diagrams.vue
@@ -22,7 +22,6 @@ const props = defineProps({
 </script>
 
 <template>
-<<<<<<< Updated upstream
     <Head title="{{ stationName }}" />
 
     <AuthenticatedLayout>
@@ -51,62 +50,9 @@ const props = defineProps({
                         title="humidity"
                         :options="props.humiditiesOptions"
                         :data="props.humiditiesData"/>
-=======
-    <Head title="Diagram"/>
 
-    <AuthenticatedLayout>
-
-        <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                Diagram
-            </h2>
-        </template>
-        
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <h1>Station Data</h1>
-
-                <!-- Zoekbalk poging 2 -->
-                <input
-                    v-model="searchQuery"
-                    type="text"
-                    placeholder="Search station id..."
-                    style="margin-bottom: 1rem; padding: 0.5rem; width: 100%; max-width: 400px; border: 1px solid #ccc; border-radius: 4px;"
-                />
-
-                <!-- Station blokken om ze te splitten vann elkaar-->
-                <div
-                    v-for="(entries, name) in filteredStations"
-                    :key="name"
-                    style="border: 1px solid #ccc; margin-bottom: 2rem; padding: 1rem; border-radius: 8px;"
-                >
-                    <h2>Station: {{ name }}</h2>
-
-                    <div style="display: flex; gap: 2rem; overflow-x: auto;">
-                        <canvas :id="`temp-${name}`" width="400" height="200"></canvas>
-                        <canvas :id="`hum-${name}`" width="400" height="200"></canvas>
-                    </div>
-                    <!--Deze 2 regels hieronder laten de raw json data van elk grafiek zien als t actief staat -->
-                    <!-- <h3>Raw Data:</h3> -->
-                    <!-- <pre>{{ JSON.stringify(entries, null, 2) }}</pre> -->
->>>>>>> Stashed changes
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
-<<<<<<< Updated upstream
-
-
 </template>
-
-=======
-</template>
-
-<script setup>
-import { onMounted, ref, computed, nextTick, watch } from 'vue'
-import axios from 'axios'
-import { Chart, registerables } from 'chart.js'
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-
->>>>>>> Stashed changes
-
